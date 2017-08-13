105f0ddbc933f632b5b5d683b3db4e1691b935bf
SDXuJun <1139809887@qq.com>
2017-08-13 20:52:32 +0800

    导入基础styl

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import Login from 'components/login/login';
 import registerServiceWorker from './registerServiceWorker';
 
+// 把一些基础样式导入
+import 'common/stylus/index.styl'
+
 ReactDOM.render(<Login />, document.getElementById('root'));
 registerServiceWorker();

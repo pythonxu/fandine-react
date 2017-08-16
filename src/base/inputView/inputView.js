655ff0e8509565843075ed2164e7292b5614ea9c
SDXuJun <1139809887@qq.com>
2017-08-17 00:02:18 +0800

    input foucs的时候添加样式

--- src/base/inputView/inputView.js
+++ src/base/inputView/inputView.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './inputView.styl'
 
-class Login extends Component {s
+class Login extends Component {
   render() {
     const {inputType, inputImg} = this.props
     return (
       <div className="inputView">
         <img src={require(`${inputImg}`)} className="input-left" ></img>
-        <input type={inputType}/>
+        <input type={inputType} className="input-center"/>
         <div className="input-right"></div>
       </div>
     )
